b4fb65a37241d7f266be5b4d5cc1ba17ed85f1fc
Heriec <2821188630@qq.com>
2025-10-26 08:38:46 +0000

    fix: prevent large subquery from causing memory overflow

--- src/backend/pilotscope/anchor2struct.c
+++ src/backend/pilotscope/anchor2struct.c
@@ -256,11 +256,30 @@ void end_anchor()
  * avoid the hash confict.
  */
 
+int is_prime(int n) {
+    if (n < 2) return 0;
+    if (n == 2) return 1;
+    if (n % 2 == 0) return 0;
+    for (int i = 3; i * i <= n; i += 2)
+        if (n % i == 0)
+            return 0;
+    return 1;
+}
+
+int next_prime(int x) {
+    if (x <= 2) return 2;
+    if (x % 2 == 0) x++;
+    while (!is_prime(x)) x += 2;
+    return x;
+}
+
 // store_aimodel_subquery2card
  void store_aimodel_subquery2card()
 {
     // avoid hash confict
-    int table_size = card_push_anchor->card_num * card_push_anchor->card_num;
+    int n = card_push_anchor->card_num;
+    int table_size = next_prime(n * 2); 
+    // int table_size = card_push_anchor->card_num * card_push_anchor->card_num;
     table      = create_hashtable(table_size);
 
     for(int i = 0;i<card_push_anchor->card_num;i++)
